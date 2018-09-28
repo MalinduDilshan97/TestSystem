5a1ac14100136f4cc497bbde9a98cc316546336e
Dhananjaya <dhananjaya@arimaclanka.com>
2018-09-28 08:14:29 +0530

    service config file added

---
+++ src/main/java/com/spring/starter/configuration/ServiceRequestIdConfig.java
@@ -0,0 +1,36 @@
+package com.spring.starter.configuration;
+
+public class ServiceRequestIdConfig {
+
+
+    public static int ISSUE_ACCAUNT_STATEMENT_FOR_PERIOD =1;
+    public static int PASSBOOK_DUPLICATE_PASSBOOK_REQUEST=2;
+    public static int PI_ACTIVE_CACEL_ESTATEMENT_FACILITY_FOR_ACCOUNTS=3;
+    public static int CHANGE_STATEMENT_FREQUENCY_TO=4;
+    public static int WITHHOLDING_TAX_DEDUCTION_CERTIFICATE=5;
+    public static int OTHER_FD_CD_RELATED_REQUESTS=6;
+    public static int DUPLICATE_FD_CD_CERTIFICATE=7;
+    public static int EFFECT_FOLLOWING_STOP_PAYMENTS=8;
+    public static int REVOKE_FOLLOWING_STOP_PAYMENTS=9;
+    public static int SUSPEND_DEBIT_CARD=10;
+    public static int REACTIVE_DEBIT_CARD=11;
+    public static int CANCEL_ATM_DEBIT_CARD=12;
+    public static int RE_ISSUE_A_PIN=13;
+    public static int SUBSCRIBE_TO_SMS_ALERTS_FOR_CARD_TRANSACTIONS=14;
+    public static int INCREASE_POS_LIMIT_OF_DEBIT_CARD=15;
+    public static int LINK_NEW_ACCAUNTS_TO_DEBIT_ATM_CARD=16;
+    public static int CHANGE_PRIMARY_ACCOUNT=17;
+    public static int CHANGE_MAILING_ADDRESS=18;
+    public static int CHANGE_PERMENT_ADDRESS=19;
+    public static int EXISTING_INTERNET_BANKING_USER_ID=20;
+    public static int REISSUE_LOGIN_PASSWORD=21;
+    public static int EXCLUDE_ACCOUNTS_FRON_INTERNET_BANKING_FACILITY=22;
+    public static int OTHER_INTERNET_BANKING_SERVICES=23;
+    public static int SUBSCRIBE_TO_SMS_ALERT=24;
+    public static int CHANGE_NIC_PASPORT_NO=25;
+    public static int CHANGE_TP_EMAIL=26;
+
+
+
+
+}
